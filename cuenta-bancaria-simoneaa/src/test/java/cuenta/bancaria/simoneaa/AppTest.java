1211366b8e4da9abf9e44a0c6be0da5790166ecf
unknown <avilarranz@gmail.com>
2026-08-19 15:24:43 +0200

    chore:creted java project

---
+++ cuenta-bancaria-simoneaa/src/test/java/cuenta/bancaria/simoneaa/AppTest.java
@@ -0,0 +1,18 @@
+package cuenta.bancaria.simoneaa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
