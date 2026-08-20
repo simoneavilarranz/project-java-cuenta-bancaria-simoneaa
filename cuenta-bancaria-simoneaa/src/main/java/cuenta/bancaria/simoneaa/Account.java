c3c9e124cf0957a5676a262a9e80fb74f93885ad
unknown <avilarranz@gmail.com>
2026-08-20 09:25:41 +0200

    feat:created account class with atributes and constructor

---
+++ cuenta-bancaria-simoneaa/src/main/java/cuenta/bancaria/simoneaa/Account.java
@@ -0,0 +1,21 @@
+package cuenta.bancaria.simoneaa;
+
+public class Account {
+
+    protected float balance;
+    protected int numberOfDeposits;
+    protected int numberOfWithdrawals;
+    protected float annualRate;
+    protected float monthlyFee;
+
+    public Account(float balance, float annualRate) {
+
+        this.balance = balance;
+        this.annualRate = annualRate;
+        this.numberOfDeposits = 0;
+        this.numberOfWithdrawals = 0;
+        this.monthlyFee = 0;
+
+    }
+    
+}
