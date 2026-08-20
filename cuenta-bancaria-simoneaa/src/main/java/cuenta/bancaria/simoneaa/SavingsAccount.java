86f058c950fa4f641de553ed0088c42e9ae42bf7
unknown <avilarranz@gmail.com>
2026-08-20 14:21:07 +0200

    feat:added override of account super class methods to savingsaccount sub class

--- cuenta-bancaria-simoneaa/src/main/java/cuenta/bancaria/simoneaa/SavingsAccount.java
+++ cuenta-bancaria-simoneaa/src/main/java/cuenta/bancaria/simoneaa/SavingsAccount.java
@@ -4,15 +4,44 @@ public class SavingsAccount extends Account {
 
     private boolean active;
 
-
     public SavingsAccount(float balance, float annualRate) {
 
         super(balance, annualRate);
 
-        if (balance >= 1000) {
+        if (balance >= 10000) {
             active = true;
-        }
+        } else active = false;
 
+    }
+
+    @Override
+    public void deposit(float amount) {
+        if (active) {
+            super.deposit(amount);
+        }
+    }
+
+    @Override
+    public void withdraw(float amount) {
+        if (active) {
+            super.withdraw(amount);
+        }
+    }
+
+    @Override
+    public void monthlyStatement() {
+        if (numberOfWithdrawals > 4) {
+            monthlyFee = monthlyFee+(numberOfWithdrawals-4)*1000;
+        } super.monthlyStatement();
+        if (balance >= 10000) {
+            active = true;
+        } else active = false;
+    }
+
+    @Override
+    public String print() {
+        String values = "Balance: " + balance + " Number of transactions: " + numberOfDeposits+numberOfWithdrawals + " Monthly fee: " + monthlyFee;
+        return values;
     }
     
 }
